d3a30afbc992faaa604fcb8216d7df9348c98e53
Cristian Gabriel <cristian_granero90@yahoo.com>
2024-02-21 20:21:19 -0300

    apiKey from hidden source

--- lib/config/helpers/movie_helper.dart
+++ lib/config/helpers/movie_helper.dart
@@ -2,13 +2,12 @@ import 'dart:async';
 
 import 'package:dio/dio.dart';
 import 'package:movies_app/infrastructure/models/movie_model.dart';
+import 'package:movies_app/keys.dart';
 import 'package:movies_app/model/entities/movie_complete.dart';
 import 'package:movies_app/model/entities/movies_list.dart';
 
 class MovieResponse {
-  final String token = 'cb86974c362f47d464bb3b6c94b8f7c2';
-  final String apiUrl =
-      'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+  final String api = apiKey;
   final _dio = Dio();
 
   Future<MoviesList> getPopularMovies(String page) async {
@@ -17,7 +16,7 @@ class MovieResponse {
         host: 'api.themoviedb.org',
         path: '3/movie/popular',
         queryParameters: {
-          'api_key': token,
+          'api_key': api,
           'include_adult': 'false',
           'page': page,
           'language': 'es-AR'
@@ -29,7 +28,7 @@ class MovieResponse {
   }
 
   void dioOptions() {
-    _dio.options.headers['Authorization'] = 'Bearer$token';
+    _dio.options.headers['Authorization'] = 'Bearer$api';
   }
 
   Future<MovieComplete> getMovie(int id) async {
@@ -37,7 +36,7 @@ class MovieResponse {
         scheme: 'https',
         host: 'api.themoviedb.org',
         path: '3/movie/$id',
-        queryParameters: {'api_key': token, 'language': 'es-AR'}).toString();
+        queryParameters: {'api_key': api, 'language': 'es-AR'}).toString();
 
     final response = await _dio.get(url);
     final movie = Result.fromJson(response.data);
